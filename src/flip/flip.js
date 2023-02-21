43fff25ab6f1cc15aed9d04a9e3af8c6ece49794
apilum <gapilsoft@gmail.com>
2023-02-21 17:34:57 +0300

    flip game styling

--- src/flip/flip.js
+++ src/flip/flip.js
@@ -1,15 +1,21 @@
 const heads = document.querySelector("#heads");
 const tails = document.querySelector("#tails");
 const resultP = document.querySelector("#result");
+const resultContainer = document.querySelector(".result-container");
 const HEADS = "Heads";
 const TAILS = "Tails";
-
+const headsList = heads.classList;
+const tailsList = tails.classList;
+const containerList = resultContainer.classList;
+var button;
 heads.addEventListener("click", function () {
   playGame2(HEADS);
+  setBorder1();
 });
 
 tails.addEventListener("click", function () {
   playGame2(TAILS);
+  setBorder2();
 });
 
 function playGame2(choice) {
@@ -25,7 +31,23 @@ function playGame2(choice) {
 
   if (choice === resultValue) {
     result.innerHTML = win;
+    containerList.remove("lose");
+
+    containerList.add("win");
   } else {
     result.innerHTML = lose;
+    containerList.remove("win");
+
+    containerList.add("lose");
   }
+}
+function setBorder1() {
+  tailsList.remove("activeButton");
+  headsList.remove("activeButton");
+  headsList.add("activeButton");
+}
+function setBorder2() {
+  headsList.remove("activeButton");
+  tailsList.remove("activeButton");
+  tailsList.add("activeButton");
 }
