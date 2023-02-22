789e26244eacd33e4acc1d51fc5331724c486c92
apilum <gapilsoft@gmail.com>
2023-02-22 23:59:16 +0300

    added prozent

--- src/flip/flip.js
+++ src/flip/flip.js
@@ -2,11 +2,15 @@ const heads = document.querySelector("#heads");
 const tails = document.querySelector("#tails");
 const resultP = document.querySelector("#result");
 const resultContainer = document.querySelector(".result-container");
+const flipTittle = document.querySelector("#flip-tittle");
 const HEADS = "Heads";
 const TAILS = "Tails";
 const headsList = heads.classList;
 const tailsList = tails.classList;
 const containerList = resultContainer.classList;
+var gameNumber = 0;
+var correctNumber = 0;
+var prozent = 0;
 var button;
 heads.addEventListener("click", function () {
   playGame2(HEADS);
@@ -19,6 +23,7 @@ tails.addEventListener("click", function () {
 });
 
 function playGame2(choice) {
+  gameNumber++;
   var randomNumber = Math.floor(Math.random() * 2);
 
   if (randomNumber === 0) {
@@ -32,14 +37,16 @@ function playGame2(choice) {
   if (choice === resultValue) {
     result.innerHTML = win;
     containerList.remove("lose");
-
     containerList.add("win");
+    correctNumber++;
   } else {
     result.innerHTML = lose;
     containerList.remove("win");
 
     containerList.add("lose");
   }
+  prozent = (correctNumber / gameNumber) * 100;
+  flipTittle.innerHTML = "Your result " + prozent.toFixed(2) + "%";
 }
 function setBorder1() {
   tailsList.remove("activeButton");
