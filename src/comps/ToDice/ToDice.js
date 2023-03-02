db3b4e17fae67c2f79f47bf266c9a74092e7bee6
Gapil <gapilsoft@gmail.com>
2023-03-02 22:31:07 +0300

    added dice game

--- src/comps/ToDice/ToDice.js
+++ src/comps/ToDice/ToDice.js
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./todice.scss";
+import point from "../../assets/score.png";
+
 import alertify from "alertifyjs";
 
 export default function ToDice() {
   const [diceNumber, setDiceNumber] = useState(1);
+  const [score, setScore] = useState(50);
+  useEffect(() => {
+    const cachedScore = localStorage.getItem("score");
+    if (cachedScore !== null) {
+      setScore(parseInt(cachedScore));
+    }
+  }, []);
 
   function rollDice() {
     let randomNumber = Math.floor(Math.random() * 6) + 1;
@@ -45,20 +54,72 @@ export default function ToDice() {
     dot.style.display = "block";
   }
   function guessNumber() {
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
     let userGuess = parseInt(document.getElementById("guessInput").value);
 
-    if (randomNumber === userGuess) {
-      alertify.set("notifier", "position", "bottom-right");
-      // alert("Congratulations! You guessed the number!");
-      alertify.success("Hey Tebrikler");
+    let randomNumberr = Math.floor(Math.random() * 6) + 1;
+    setDiceNumber(randomNumberr);
+    let dots = document.querySelectorAll(".dot");
+    dots.forEach((dot) => (dot.style.display = "none"));
+    if (randomNumberr === 1) {
+      showDot(5);
+    } else if (randomNumberr === 2) {
+      showDot(2);
+      showDot(3);
+    } else if (randomNumberr === 3) {
+      showDot(2);
+      showDot(3);
+      showDot(5);
+    } else if (randomNumberr === 4) {
+      showDot(1);
+      showDot(2);
+      showDot(3);
+      showDot(4);
+    } else if (randomNumberr === 5) {
+      showDot(1);
+      showDot(2);
+      showDot(3);
+      showDot(4);
+      showDot(5);
+    } else if (randomNumberr === 6) {
+      showDot(1);
+      showDot(2);
+      showDot(3);
+      showDot(4);
+      showDot(6);
+      showDot(7);
+    } //
+
+    if (randomNumberr === userGuess) {
+      alertify.success("Congrats!");
+      setScore(score + 20);
+      localStorage.setItem("score", score + 20);
     } else {
-      alertify.error("Başaramadık");
+      alertify.error(`Ups! Unlucky, the number was  ${randomNumberr}.`);
+      setScore(score - 10);
+      localStorage.setItem("score", score - 10);
     }
   }
+  function handleKeyPress(event) {
+    if (event.key === "Enter") {
+      guessNumber();
+    } else if (isNaN(parseInt(event.key, 10))) {
+      event.preventDefault();
+    }
+  }
+  useEffect(() => {
+    const cachedScore = localStorage.getItem("score");
+    if (cachedScore !== null) {
+      setScore(parseInt(cachedScore));
+    }
+  }, []);
 
   return (
     <div className="ToDice">
+      <div id="score">
+        <img src={point} alt="" width="120px" height="80px" />
+
+        <h1>Your Score: {score}</h1>
+      </div>
       <div className="dice-container">
         <div className="dice">
           <div className="dot"></div>
@@ -69,19 +130,22 @@ export default function ToDice() {
           <div className="dot"></div>
           <div className="dot"></div>
         </div>
-      </div>
-      <p>Dice Number: {diceNumber}</p>
-      <button onClick={rollDice}>Roll Dice</button>
-      <div>
-        <input type="number" id="guessInput" />
-        <button
-          onClick={() => {
-            guessNumber();
-            rollDice();
-          }}
-        >
-          Guess the Number
-        </button>
+        <div className="buttons">
+          <button className="enjoy" onClick={rollDice}>
+            Roll Dice
+          </button>
+
+          <div className="guess">
+            <button
+              onClick={() => {
+                guessNumber();
+              }}
+            >
+              Guess!
+            </button>
+            <input type="number" id="guessInput" onKeyPress={handleKeyPress} />
+          </div>
+        </div>
       </div>
     </div>
   );
