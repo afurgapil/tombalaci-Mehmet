7f25f374d6291664600c0691661f51b503403299
apilum <gapilsoft@gmail.com>
2023-03-18 05:54:51 +0300

    updated header design

--- src/comps/Header/Header.js
+++ src/comps/Header/Header.js
@@ -1,49 +1,56 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./header.scss";
 import logo from "../../assets/logo.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <ul>
-            <li className="logo">
-              <img src={logo} alt="" width="120px" height="80px" />
-            </li>
-            <div className="header-right">
-              <li className="header-right-item">
-                <Link to="/">Home</Link>
-              </li>
-              {/* <li className="header-right-item">
-                <Link to="/tombala">Tombala</Link>
-              </li> */}
-              <li className="header-right-item">
-                <Link to="/coinflip">Coin Flip</Link>
-              </li>
-              <li className="header-right-item">
-                <Link to="/todice">To Dice</Link>
-              </li>
-              <li className="header-right-item">
-                <Link to="/rps">RPS</Link>
-              </li>
-              <li className="header-right-item">
-                <Link to="/roulette">Roulette</Link>
-              </li>
-              <li className="header-right-item">
-                <Link to="/slot">Slot</Link>
-              </li>{" "}
-              <li className="header-right-item">
-                <Link to="/emojify">Emojify</Link>
-              </li>
-              <li className="header-right-item">
-                <Link to="/contact">Contact</Link>
-              </li>
-            </div>
-          </ul>
-        </header>
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="header-container">
+      <div className="logo-container">
+        <img src={logo} alt="logo" />
+        <div className="menu-icon">
+          <MenuIcon onClick={toggleMenu} color="primary"></MenuIcon>
+        </div>
       </div>
-    );
-  }
+      <div className="menu-container">
+        <ul className={`navigation ${isMenuOpen ? "open" : ""}`}>
+          <li>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/coinflip">CoinFlip</NavLink>
+          </li>
+          <li>
+            <NavLink to="/todice">ToDice</NavLink>
+          </li>
+          <li>
+            <NavLink to="/rps">RPS</NavLink>
+          </li>
+          <li>
+            <NavLink to="/roulette">Roulette</NavLink>
+          </li>
+          <li>
+            <NavLink to="/slot">Slot</NavLink>
+          </li>
+          <li>
+            <NavLink to="/emojify">Emojify</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
+
+export default Header;
