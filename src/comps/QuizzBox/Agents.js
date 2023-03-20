e74248e0ea81b7b5219685b5ab87551d5de12e98
apilum <gapilsoft@gmail.com>
2023-03-20 21:10:25 +0300

    styling

--- src/comps/QuizzBox/Agents.js
+++ src/comps/QuizzBox/Agents.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import agents from "../../data/agents";
 import alertify from "alertifyjs";
 import "./agents.scss";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function Agents() {
   const [selectedAgents, setSelectedAgents] = useState([]);
@@ -43,43 +45,55 @@ function Agents() {
   };
 
   return (
-    <div>
-      <h1>Valorant Agent Guessing Game</h1>
+    <div id="valodle-game-container">
+      <h1 id="vaoldle-title">Valorant Agent Guessing Game</h1>
       <div id="agent-input">
-        <label>Choose an Agent:</label>
-        <input type="text" value={userInput} onChange={handleChange} />
-        <button onClick={handleAddItem}>Add Item</button>
+        <TextField
+          id="filled-basic"
+          label="Choose an Agent"
+          variant="filled"
+          type="text"
+          value={userInput}
+          onChange={handleChange}
+        />
+
+        <Button variant="contained" onClick={handleAddItem}>
+          Check
+        </Button>
+      </div>
+      <div id="gecici">
         <p>User Input: {userInput}</p>
         <p>last Input: {lastAgent}</p>
         <p>Selected Agents: {selectedAgents}</p>
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Gender</th>
-              <th>Role</th>
-              <th>Species</th>
-              <th>Region</th>
-            </tr>
-          </thead>
-          <tbody>
-            {selectedAgents.map((agent, index) => {
-              const selectedAgentData = agents.find((a) => a.name === agent);
-              if (!selectedAgentData) return null;
-              return (
-                <tr key={index}>
-                  <td>{selectedAgentData.name}</td>
-                  <td>{selectedAgentData.gender}</td>
-                  <td>{selectedAgentData.role}</td>
-                  <td>{selectedAgentData.species}</td>
-                  <td>{selectedAgentData.region}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <p>Computer Agent: {computerAgent}</p>
       </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Gender</th>
+            <th>Role</th>
+            <th>Species</th>
+            <th>Region</th>
+          </tr>
+        </thead>
+        <tbody>
+          {selectedAgents.map((agent, index) => {
+            const selectedAgentData = agents.find((a) => a.name === agent);
+            if (!selectedAgentData) return null;
+            return (
+              <tr key={index}>
+                <td id="table-name">{selectedAgentData.name}</td>
+                <td>{selectedAgentData.gender}</td>
+                <td>{selectedAgentData.role}</td>
+                <td>{selectedAgentData.species}</td>
+                <td>{selectedAgentData.region}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <p>Computer Agent: {computerAgent}</p>
     </div>
   );
 }
