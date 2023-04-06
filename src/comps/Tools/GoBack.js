dc38206f8e4b35011b3b8d88f3698079d4684e92
apilum <gapilsoft@gmail.com>
2023-04-07 02:53:40 +0300

    updated goback tool

--- src/comps/Tools/GoBack.js
+++ src/comps/Tools/GoBack.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa";
 import "./goback.scss";
 import "animate.css/animate.min.css";
 
 function GoBack() {
-  const handleClick = () => {
-    window.history.back();
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1);
   };
 
   return (
     <button
       className="go-back animate__animated animate__bounce animate__infinite animate__slower"
-      onClick={handleClick}
+      onClick={handleGoBack}
     >
       <FaArrowLeft />
     </button>
