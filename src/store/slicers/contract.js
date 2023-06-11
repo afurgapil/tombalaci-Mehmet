f80cec7164ad7c9e3c4b582268d94a3274fb3298
apilum <gapilsoft@gmail.com>
2023-06-12 02:07:40 +0300

    added wheel contract store

--- src/store/slicers/contract.js
+++ src/store/slicers/contract.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   donate: null,
+  wheel: null,
 };
 
 export const contractsSlice = createSlice({
@@ -11,9 +12,12 @@ export const contractsSlice = createSlice({
     setDonateContract: (state, action) => {
       state.donate = action.payload;
     },
+    setWheelContract: (state, action) => {
+      state.wheel = action.payload;
+    },
   },
 });
 
-export const { setDonateContract } = contractsSlice.actions;
+export const { setDonateContract, setWheelContract } = contractsSlice.actions;
 
 export default contractsSlice.reducer;
